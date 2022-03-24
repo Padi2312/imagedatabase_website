5ef855c28ded9749dfaf098f15246eb8481c3486
Mirco Messerschmidt <75323003+mircomesserschmidt@users.noreply.github.com>
2022-03-24 22:03:42 +0100

    sql skript + typeorm db connection + entities

--- backend/src/main.module.ts
+++ backend/src/main.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UploadModule } from './upload/upload.module';
 
 @Module({
-    imports: [
+    imports: [ 
+        TypeOrmModule.forRoot({
+            /*
+            type: 'mysql',
+            host: 'mysql.int.parndt.de',
+            port: 3306,
+            username: 'root',
+            password: 'mysql',
+            database: 'empstamp',  
+            */
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: '',
+            database: 'webservice',
+            entities: []
+        }),
         UploadModule
     ],
     controllers: [],
